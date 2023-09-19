54ca4e064495f72c26d54a8aed78525c7c8a18da
Najid Navas <najidnavas2000@gmail.com>
2023-09-19 05:54:06 +0000

    update teh code

--- cycle1/dfa00substring.c
+++ cycle1/dfa00substring.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
+
 #define MAX 100
 
 void main() {
-char str[MAX];
-int i, state = 0;
+  char str[MAX];
+  int i, state = 0;
 
-printf("Enter a string to test: ");
-scanf("%s", str);
+  printf("Enter a string to test: ");
+  scanf("%s", str);
 
-for(i = 0; str[i] != '\0'; i++) {
-switch(state) {
-case 0: if(str[i] == '0') state = 1;
-else state = 0;
-break;
+  for (i = 0; str[i] != '\0'; i++) {
+    switch (state) {
+    case 0:
+      if (str[i] == '0') state = 1;
+      else state = 0;
+      break;
 
-case 1: if(str[i] == '0') state = 2;
-else state = 0;
-break;
+    case 1:
+      if (str[i] == '0') state = 2;
+      else state = 0;
+      break;
 
-case 2: break;
-}
+    case 2:
+      break;
+    }
 
-if(state == 2) {
-printf("String accepted\n");
-return;
-}
-}
+    if (state == 2) {
+      printf("String accepted\n");
+      return;
+    }
+  }
 
-printf("String not accepted\n");
+  printf("String not accepted\n");
 }
