37c3862edc3894b1e43d9ba347239d92e5d49135
Najid Navas <najidnavas2000@gmail.com>
2023-09-19 05:50:59 +0000

    updated the code

--- cycle1/dfa00substring.c
+++ cycle1/dfa00substring.c
@@ -1,34 +1,31 @@
 #include <stdio.h>
-// create a DFA that accepts strings that contain the substring 00
+#define MAX 100
 
-int main(){
-  // accept the sttring that contain 00 as a substring together wiith no other character in the middle of  00
-  char str[100];
-  printf("Enter the string: ");
-  scanf("%s", str);
-  int state = 0;
-  int i = 0;
-  while(str[i] != '\0'){
-    if(state == 0 && str[i] == '0'){
-      state = 1;
-    }
-    else if(state == 1 && str[i] == '0'){
-      state = 2;
-    }
-    else if(state == 2 && str[i] == '0'){
-      state = 2;
-    }
-    else{
-      state = 0;
-    }
-    i++;
-  }
-  if(state == 2){
-    printf("Accepted\n");
-  }
-  else{
-    printf("Not Accepted\n");
-  }
-  return 0;
+void main() {
+char str[MAX];
+int i, state = 0;
 
+printf("Enter a string to test: ");
+scanf("%s", str);
+
+for(i = 0; str[i] != '\0'; i++) {
+switch(state) {
+case 0: if(str[i] == '0') state = 1;
+else state = 0;
+break;
+
+case 1: if(str[i] == '0') state = 2;
+else state = 0;
+break;
+
+case 2: break;
+}
+
+if(state == 2) {
+printf("String accepted\n");
+return;
+}
+}
+
+printf("String not accepted\n");
 }
