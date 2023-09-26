2a5fc6cdd2099856d0421103f26a2433a4726881
Najid Navas <najidnavas2000@gmail.com>
2023-09-26 04:44:23 +0000

    added the code

---
+++ cycle1/exp3.c
@@ -0,0 +1,38 @@
+// c program for implenting a dfa for accepting the binary string ending with 00
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+#define MAX 100
+
+void main(){
+  char str[MAX];
+  int i, state = 0;
+  printf("Enter a string to test: ");
+  scanf("%s", str);
+  for (i = 0; str[i] != '\0'; i++) {
+    switch (state) {
+    case 0:
+      if (str[i] == '0') state = 1;
+      else state = 0;
+      break;
+
+    case 1:
+      if (str[i] == '0') state = 2;
+      else state = 0;
+      break;
+
+    case 2:
+      break;
+    }
+
+    if (state == 2) {
+      printf("String accepted\n");
+      return;
+    }
+    
+  }
+  printf("String not accepted\n");
+}
