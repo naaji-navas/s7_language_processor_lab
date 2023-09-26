f1a2c92a5c9829b3f06113abb37b14eae9e6870c
Najid Navas <najidnavas2000@gmail.com>
2023-09-26 04:49:49 +0000

    code for implenting to test whether an input string ihaving abc as a substing

---
+++ cycle1/exp4.c
@@ -0,0 +1,45 @@
+// c program for implenting a dfa for accepting strings from the set {a,b,c} having abc as substring
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+#define MAX 100
+
+void main(){
+  char str[MAX];
+  int i, state = 0;
+  printf("Enter a string to test: ");
+  scanf("%s", str);
+  for (i = 0; str[i] != '\0'; i++) {
+    switch (state) {
+    case 0:
+      if (str[i] == 'a') state = 1;
+      else state = 0;
+      break;
+
+    case 1:
+      if (str[i] == 'b') state = 2;
+      else if (str[i] == 'a') state = 1;
+      else state = 0;
+      break;
+
+    case 2:
+      if (str[i] == 'c') state = 3;
+      else if (str[i] == 'a') state = 1;
+      else state = 0;
+      break;
+
+    case 3:
+      break;
+    }
+
+    if (state == 3) {
+      printf("String accepted\n");
+      return;
+    }
+    
+  }
+  printf("String not accepted\n");
+}
